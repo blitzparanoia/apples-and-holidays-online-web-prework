04af3e3b22598ac880f7efae66be9f43ab0683a5
Irene Saez <bluemilk44@gmail.com>
2019-02-02 01:07:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.map do |season, holidays|
-    holidays.map do |holiday_name, 
+    holidays.map do |holiday, supplies|
+      
   
 
 end
