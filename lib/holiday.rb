5d9a4ae50793d38e986d134bafbbf2261c98e6ef
Irene Saez <bluemilk44@gmail.com>
2019-02-02 00:55:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |holiday, supply|
   puts "#{season.capitalize}"
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
