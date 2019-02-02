2ec7fc7e3e114ee5ab730f9e1970df68434c7fec
Irene Saez <bluemilk44@gmail.com>
2019-02-02 00:59:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
-holiday_hash.each do |holiday, 
+holiday_hash.each do |holiday, supplies|
+  
 end
 end
 
